f4a7114aa83bca5404d2ad1e3666e1eff63215e1
brunamouraa <80169032+brunamouraa@users.noreply.github.com>
2021-05-23 21:39:32 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,13 @@
+function cadastro() {
+    if (document.getElementById("cadastro").style.display = "block") {
+        document.getElementById("login").style.display = "none";
+    }   
+}
+
+function login() {
+    if (document.getElementById("cadastro").style.display = "none") {
+        
+        document.getElementById("login").style.display = "block";
+        
+    }
+}
